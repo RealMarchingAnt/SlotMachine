de1e5c3620164bd26a687f95ff2ff61370a770d8
RealMarchingAnt <patrikjohansson@Patriks-MacBook-Pro.local>
2014-10-23 22:44:58 +0200

    Project finished

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Patrik Johansson on 20/10/14.
+//  Copyright (c) 2014 PJ Games. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
